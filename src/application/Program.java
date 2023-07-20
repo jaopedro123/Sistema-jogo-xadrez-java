e48719c776799cda86e2f692a684b4b106458fe9
joao pedro <joao.ferreira01br@gmail.com>
2023-07-20 12:16:45 -0300

    Colocando na tela os possiveis movimentos

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,7 +22,10 @@ public class Program {
 				System.out.println();
 				System.out.print("Source:");
 				ChessPosition source = UI.readChessPosition(sc);
-
+				
+				boolean[][] possibleMoves = chessmatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessmatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target:");
 				ChessPosition target = UI.readChessPosition(sc);
